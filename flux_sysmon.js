7957b451c9aeb6a09f8ec4fcb2522636e1f80777
John K <john@j-k-tuning.com>
2022-11-04 21:25:49 -0400

    trim whitespace

--- flux_sysmon.js
+++ flux_sysmon.js
@@ -3,18 +3,18 @@ const shell = require('shelljs');
 const fs = require('fs');
 const webhookClient = new WebhookClient({ url: 'URL' });
 
-var disku_max = shell.exec(`df -Hl / | grep -v File | tr -s ' '|cut -f2 -d" "`,{ silent: true }).stdout;
-var disku_per = shell.exec(`df -Hl / | grep -v File | tr -s ' '|cut -f5 -d" "`,{ silent: true }).stdout;
+var disku_max = shell.exec(`df -Hl / | grep -v File | tr -s ' '|cut -f2 -d" "`,{ silent: true }).stdout.trim();
+var disku_per = shell.exec(`df -Hl / | grep -v File | tr -s ' '|cut -f5 -d" "`,{ silent: true }).stdout.trim();
 
 const embed = new EmbedBuilder()
-    .setTitle('Disk Usage Report')
+    .setTitle(`Disk Usage Report`)
     .setColor(0x00FFFF)
-    .addFields({ name: 'Disk Usage ', value: `${disku_per} of ${disku_max}` });
+    .addFields({ name: `Disk Usage `, value: `${disku_per} of ${disku_max}` });
 
 webhookClient.send({
-	content: 'Flux Node System Report',
-	username: 'FluxNode',
-	avatarURL: 'https://i.imgur.com/AfFp7pu.png',
+	content: `Flux Node System Report`,
+	username: `FluxNode`,
+	avatarURL: `https://i.imgur.com/AfFp7pu.png`,
 	embeds: [embed],
 });
 

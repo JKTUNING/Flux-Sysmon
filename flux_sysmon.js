60d0de3bf9fb2d89dec705e0a1f768adf4d82e8c
John K <john@j-k-tuning.com>
2022-11-04 22:53:58 -0400

    add Memory usage

--- flux_sysmon.js
+++ flux_sysmon.js
@@ -6,12 +6,20 @@ const webhookClient = new WebhookClient({ url: 'URL' });
 var disku_max = shell.exec(`df -Hl / | grep -v File | tr -s ' '|cut -f2 -d" "`,{ silent: true }).stdout.trim();
 var disku_per = shell.exec(`df -Hl / | grep -v File | tr -s ' '|cut -f5 -d" "`,{ silent: true }).stdout.trim();
 var Hostname = shell.exec(`hostname`,{ silent: true }).stdout.trim();
+var memTotal = shell.exec(`cat /proc/meminfo | grep MemTotal | awk -F ':' '{print $2}' | awk -F ' kB' '{print $1}' `,{ silent: true}).stdout.trim();
+var memAvailable = shell.exec(`cat /proc/meminfo | grep MemAvailable | awk -F ':' '{print $2}' | awk -F ' kB' '{print $1}' `,{ silent: true}).stdout.trim();
+
+var memUsed = memTotal-memAvailable
+var memPercent = memTotal/memUsed
 
 const embed = new EmbedBuilder()
-    .setTitle(`Disk Usage Report`)
-    .setColor(0xff0000)
-	.addFields({ name: `Hostname`, value: `${Hostname}`, inline: true })
-    .addFields({ name: `Usage of /:`, value: `${disku_per} of ${disku_max}`, inline: true });
+	.setTitle(`Disk Usage Report`)
+	.setColor(0xff0000)
+	.addFields({ name: `Host`, value: `${Hostname}`, inline: true })
+	.addFields({ name: `Usage of /:`, value: `${disku_per} of ${disku_max}`, inline: true })
+	.addFields({ name: `% MEM USED :`, value: `${memPercent}`, inline: true })
+	.addFields({ name: `MEMORY TOTAL:`, value: `${memTotal}` })
+	.addFields({ name: `MEMPORY AVAILABLE:`, value: `${memAvailable}` });
 
 webhookClient.send({
 	username: `FluxNode`,

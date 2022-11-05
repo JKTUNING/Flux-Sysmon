71fd64f031a8034284c6421dc62a9bd654f03b4c
John K <john@j-k-tuning.com>
2022-11-04 20:06:19 -0400

    downgrade discord.js

--- flux_sysmon.js
+++ flux_sysmon.js
@@ -1,14 +1,14 @@
 const { EmbedBuilder, WebhookClient } = require('discord.js');
 const shell = require('shelljs');
 const fs = require('fs');
-const webhookClient = new WebhookClient({ url: 'WEBHOOK_URL' });
+const webhookClient = new WebhookClient({ url: 'URL' });
 
 var disku_max = shell.exec(`df -Hl / | grep -v File | tr -s ' '|cut -f2 -d" "`,{ silent: true }).stdout;
 var disku_per = shell.exec(`df -Hl / | grep -v File | tr -s ' '|cut -f5 -d" "`,{ silent: true }).stdout;
 
 const embed = new EmbedBuilder()
-	.setTitle('Disk Usage Report')
-	.setColor(0x00FFFF)
+    .setTitle('Disk Usage Report')
+    .setColor(0x00FFFF)
     .addFields({ name: 'Disk Usage', value: `${disku_per} of ${disku_max}`, inline: true });
 
 webhookClient.send({

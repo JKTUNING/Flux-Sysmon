ee606b0dcd598156f8790d5e15f3c2fa0de3399b
John K <john@j-k-tuning.com>
2022-11-21 23:00:26 -0500

    Daily usage report

--- flux_sysmon.js
+++ flux_sysmon.js
@@ -5,17 +5,25 @@ const fs = require('fs');
 var cron = require('node-cron');
 const webhookClient = new WebhookClient({ url: webhookURL });
 
+var Hostname="";
+var disku_max="";
+var disku_per="";
+var diskPercent="";
+var memTotal="";
+var memAvailable ="";
+var memPercent="";
+
 cron.schedule('*/15 * * * *', () => {
 
-	var Hostname = shell.exec(`hostname`,{ silent: true }).stdout.trim();
+	Hostname = shell.exec(`hostname`,{ silent: true }).stdout.trim();
 
-	var disku_max = shell.exec(`df -Hl / | grep -v File | tr -s ' '|cut -f2 -d" "`,{ silent: true }).stdout.trim();
-	var disku_per = shell.exec(`df -Hl / | grep -v File | tr -s ' '|cut -f5 -d" "`,{ silent: true }).stdout.trim();
-	var diskPercent = Math.floor(disku_per.replace('%', ''));
+	disku_max = shell.exec(`df -Hl / | grep -v File | tr -s ' '|cut -f2 -d" "`,{ silent: true }).stdout.trim();
+	disku_per = shell.exec(`df -Hl / | grep -v File | tr -s ' '|cut -f5 -d" "`,{ silent: true }).stdout.trim();
+	diskPercent = Math.floor(disku_per.replace('%', ''));
 
-	var memTotal = shell.exec(`cat /proc/meminfo | grep MemTotal | awk -F ':' '{print $2}' | awk -F ' kB' '{print $1}' `,{ silent: true}).stdout.trim();
-	var memAvailable = shell.exec(`cat /proc/meminfo | grep MemAvailable | awk -F ':' '{print $2}' | awk -F ' kB' '{print $1}' `,{ silent: true}).stdout.trim();
-	var memPercent = Math.floor(((memTotal-memAvailable) / memTotal) * 100);
+	memTotal = shell.exec(`cat /proc/meminfo | grep MemTotal | awk -F ':' '{print $2}' | awk -F ' kB' '{print $1}' `,{ silent: true}).stdout.trim();
+	memAvailable = shell.exec(`cat /proc/meminfo | grep MemAvailable | awk -F ':' '{print $2}' | awk -F ' kB' '{print $1}' `,{ silent: true}).stdout.trim();
+	memPercent = Math.floor(((memTotal-memAvailable) / memTotal) * 100);
 
 	console.log(`USAGE OF /: ${disku_per} of ${disku_max}`);
 	console.log(`MEMORY USED : ${memPercent}%`);
@@ -60,6 +68,25 @@ cron.schedule('*/15 * * * *', () => {
 		console.log(`MONESTRY IMAGE NOT FOUND`);
 	}
 	console.log(`#########################################`);
+});
+
+
+const cron2 = cron.schedule('* 0 * * *', () => {
+
+	const embed = new EmbedBuilder()
+		.setTitle(`Daily Machine Usage Report`)
+		.setColor(0xff0000)
+		.addFields({ name: `Host`, value: `${Hostname}` })
+		.addFields({ name: `Usage of /:`, value: `${disku_per} of ${disku_max}` })
+		.addFields({ name: `MEMORY USED :`, value: `${memPercent}%` })
+		.addFields({ name: `MEMORY TOTAL:`, value: `${memTotal}` })
+		.addFields({ name: `MEMORY AVAILABLE:`, value: `${memAvailable}` });
+
+		webhookClient.send({
+			username: `FluxNode`,
+			avatarURL: `https://i.imgur.com/AfFp7pu.png`,
+			embeds: [embed]
+		});
 });
 
 //nvm install 16

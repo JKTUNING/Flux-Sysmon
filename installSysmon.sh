f08fae85acf666ed8d5abb260629ff15cf7795b9
John K <john@j-k-tuning.com>
2022-12-09 15:47:30 -0500

    remove sudo

--- installSysmon.sh
+++ installSysmon.sh
@@ -5,10 +5,10 @@ git checkout dev
 
 
 if ! [[ -f config.json ]]; then
-    sudo touch config.json
+    touch config.json
     userWebHook=$(whiptail --inputbox "Enter your Discord Webhook URL" 8 60 3>&1 1>&2 2>&3)
     userNick=$(whiptail --inputbox "Enter your Discord USER ID" 8 60 3>&1 1>&2 2>&3)
-sudo bash -c cat > config.json <<EOF
+    cat > config.json <<EOF
 {
   "webhookURL" : "$userWebHook",
   "userID" : "$userNick"

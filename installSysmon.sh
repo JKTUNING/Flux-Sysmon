8fe9b2a12179272f4e6bea168ce54d30ee4d49ee
John K <john@j-k-tuning.com>
2022-12-09 15:36:53 -0500

    elevate priv

--- installSysmon.sh
+++ installSysmon.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #switch to dev branch
-git checkout dev
+#git checkout dev
 
 
 if ! [[ -f config.json ]]; then
     sudo touch config.json
     userWebHook=$(whiptail --inputbox "Enter your Discord Webhook URL" 8 60 3>&1 1>&2 2>&3)
     userNick=$(whiptail --inputbox "Enter your Discord USER ID" 8 60 3>&1 1>&2 2>&3)
-cat > config.json <<EOF
+sudo bash -c cat > config.json <<EOF
 {
   "webhookURL" : "$userWebHook",
   "userID" : "$userNick"
@@ -18,6 +18,6 @@ else
     echo -e "config file found - not entry needed"
 fi
 
-npm install
+#npm install
 
-pm2 start flux_sysmon.js --watch
+#pm2 start flux_sysmon.js --watch

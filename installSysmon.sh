bcba6fd9284e3a87cb11d0e6d14e25143b7b7874
JKTUNING <jktuned.crypto@gmail.com>
2022-12-11 11:06:07 -0500

    updarte installer

--- installSysmon.sh
+++ installSysmon.sh
@@ -21,6 +21,14 @@ else
     echo -e "packages aleady exist .. npm install skipped"
 fi
 
+#check to see if flux_sysmon is already running and stop/delete it from pm2
+if [[ $(pm2 info flux_sysmon 2>&1 | grep status) != "" ]]; then 
+    pm2 stop --silent flux_sysmon
+    sleep 1
+    pm2 delete --silent flux_sysmon
+    sleep 1
+fi
+
 pm2 start flux_sysmon.js --watch
 sleep 2
 pm2 save

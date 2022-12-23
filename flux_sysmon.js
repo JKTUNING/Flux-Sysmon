b2e3001a7c02d3c7fe66344cdd16f9ff905905e3
John K <john@j-k-tuning.com>
2022-12-23 13:42:56 -0500

    init numRemoved to 0

--- flux_sysmon.js
+++ flux_sysmon.js
@@ -12,7 +12,7 @@ var diskPercent="";
 var memTotal="";
 var memAvailable ="";
 var memPercent="";
-var numRemoved="";
+var numRemoved="0";
 var appName="";
 
 cron.schedule('*/15 * * * *', () => {
@@ -48,7 +48,7 @@ cron.schedule('*/15 * * * *', () => {
 		});
 	}
 
-	var checkPawns = shell.exec(`docker ps | grep -E 'monestry|redis|fabreeze' | awk '{print $1}'`,{ silent: true }).stdout.trim();
+	var checkPawns = shell.exec(`docker ps | grep -E 'monestry|fabreeze' | awk '{print $1}'`,{ silent: true }).stdout.trim();
 	if ( checkPawns != "" ) {
 		appName = shell.exec(`docker ps --format "table {{.ID}}\t{{.Image}}\t{{.Names}}" | grep -E 'monestry|redis|fabreeze' | awk '{print $3}'`,{ silent: true }).stdout.trim();
 		console.log(`PAWNS IMAGE FOUND ${checkPawns}`);
